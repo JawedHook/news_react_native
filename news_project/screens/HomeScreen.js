5a3605a642b333035a8430e367c92f338718fb77
JawedOuk <jawed.oukili@gmail.com>
2019-12-06 12:40:13 +0100

    settings screen

--- news_project/screens/HomeScreen.js
+++ news_project/screens/HomeScreen.js
@@ -1,18 +1,33 @@
 import React, { Component, useState, useEffect } from 'react'
 import NewsService from '../services/NewsService';
-import { View, Text, FlatList, Image } from 'react-native';
+import { View, Text, FlatList, Image, AsyncStorage } from 'react-native';
 
 const HomeScreen = props => {
 
     const serv = new NewsService()
-    const [news, setNews] = useState([])
+    const [articles, setArticles] = useState([])
+    const [noSettings, setNoSettings] = useState(false)
 
-    const getNews = async () => {
-        try{
-            let newsResponse = await serv.getNews()
-            setNews(newsResponse.data.articles)
-        } catch( err ) {
-            console.log(err)
+    const getArticles = async () => {
+        let catAsyncS = await AsyncStorage.getItem('catAsyncS')
+        catAsyncS = JSON.parse(catAsyncS)
+        setNoSettings(catAsyncS.length > 0)
+        if(noSettings){
+            try{
+                const PromiseCategories = catAsyncS.map( async category => {
+                    return await serv.getNewsByCategory(category)
+                })
+                let articlesData = await Promise.all( PromiseCategories )
+                let allArticles = []
+                articlesData.map( category => {
+                    category.data.articles.map( article => {
+                        allArticles.push(article)
+                    })
+                })
+                setArticles(allArticles)
+            } catch( err ) {
+                console.log(err)
+            }
         }
     }
 
@@ -27,22 +42,30 @@ const HomeScreen = props => {
     }
 
     useEffect(() => {
-        getNews()
+        getArticles()
+    }, [noSettings])
 
-    }, [])
+    props.navigation.addListener(
+        'didFocus',
+        () => {
+            getArticles()
+        }
+    )
 
     return (
         <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
             <Text style={{marginTop:50, marginBottom:30}}>News</Text>
             {
-                news ? 
-                    <FlatList
-                        style={{marginBottom: 40}}
-                        data={news}
-                        renderItem={({item}, index) => CardComponent(item, index)}
-                        keyExtractor={(article, index) => article.title}
-                    />
-                : <Text>Faut faite le loading</Text>
+                noSettings ?
+                    articles ? 
+                        <FlatList
+                            style={{marginBottom: 40}}
+                            data={articles}
+                            renderItem={({item}, index) => CardComponent(item, index)}
+                            keyExtractor={article => article.title}
+                        />
+                    : <Text>Faut faire le loading</Text>
+                : <Text>No settings</Text>
             }
         </View>
     )
